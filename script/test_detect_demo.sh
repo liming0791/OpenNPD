3d0754669bd22abc3b4a81a0f1bc279c580537a0
liming <2440049438@qq.com>
2018-05-25 16:56:04 +0800

    fix files

--- script/test_detect_demo.sh
+++ script/test_detect_demo.sh
@@ -1,4 +1,7 @@
 SH_FILE=$(readlink -f $0)
 SH_DIR=$(dirname $SH_FILE)
 cd $SH_DIR 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/../lib
 ../bin/test_detect ../models/frontal_face_detector.bin ../data/sample_images/facedetect_5.jpg
+mkdir ../test_results
+mv *.jpg ../test_results/
